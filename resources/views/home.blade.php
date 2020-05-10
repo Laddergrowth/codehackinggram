e6a2551d79746a3467cb95942504533b46531c3a
canario <45767956+Laddergrowth@users.noreply.github.com>
2020-05-10 19:00:39 +0100

    updating
    site parameters and functionalidade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,9 @@
                     <img src="https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-19/s150x150/87604514_848249612269338_6257783984680337408_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_ohc=qdHzj1kOrF0AX_C-G8g&oh=922bbdce3e7c462ac0f0c82ddc91947b&oe=5EDFC47A" class="rounded-circle">
         </div>
     <div class="col-9 pt-5">
-        <div><h1>CodingHackingGram</h1></div>
+
+        <div><h1>{{ $user->username }}</h1></div>
+
         <div class="d-flex">
             <div class="pr-4"><strong>2</strong> posts</div>
             <div class="pr-4"><strong>2</strong> followers</div>
@@ -16,6 +18,18 @@
         <div class="pt-4" font-weight-> emvoltadaingla </div>
         <div class="pt-4"> Help our channel to grow so we can play more and give more games:) 🎮⚽🎮 </div>
 
+        <div> <a href="#">CodeHakingGram</a></div>
+
+        <div class="row pt-5">
+            <div class="col-4">
+              <img src="https://images.unsplash.com/photo-1589107736323-462d51b898e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" class="w-100">
+            </div>
+             <div class="col-4">
+              <img src="https://images.unsplash.com/photo-1589107736323-462d51b898e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" class="w-100">
+            </div>
+              <div class="col-4">
+              <img src="https://images.unsplash.com/photo-1589107736323-462d51b898e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" class="w-100">
+            </div>
 
     </div>
 </div>
