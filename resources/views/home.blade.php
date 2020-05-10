3eb67ecc657f6331a4b507fed8d241305315fb9a
canario <45767956+Laddergrowth@users.noreply.github.com>
2020-05-10 13:58:27 +0100

    building poje
    Gram dev

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-3 p-5">
+                    <img src="https://scontent-lhr8-1.cdninstagram.com/v/t51.2885-19/s150x150/87604514_848249612269338_6257783984680337408_n.jpg?_nc_ht=scontent-lhr8-1.cdninstagram.com&_nc_ohc=qdHzj1kOrF0AX_C-G8g&oh=922bbdce3e7c462ac0f0c82ddc91947b&oe=5EDFC47A" class="rounded-circle">
+        </div>
+    <div class="col-9 pt-5">
+        <div><h1>CodingHackingGram</h1></div>
+        <div class="d-flex">
+            <div class="pr-4"><strong>2</strong> posts</div>
+            <div class="pr-4"><strong>2</strong> followers</div>
+            <div class="pr-4"><strong>16</strong> following</div>
+        </div>
+        <div class="pt-4" font-weight-> emvoltadaingla </div>
+        <div class="pt-4"> Help our channel to grow so we can play more and give more games:) 🎮⚽🎮 </div>
+
+
+    </div>
+</div>
+@endsection
